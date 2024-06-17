90fcf453e498bc5c661045409dafb9732ade0b95
MDorizon <mdorizon@proton.me>
2024-06-17 13:33:26 +0200

    :construction: posts listing

--- src/index.php
+++ src/index.php
@@ -1,6 +1,19 @@
-
 <?php require_once 'parts/header.php'; ?>
+<?php 
+    // connect to db
+    $connectDatabase = new PDO("mysql:host=db;dbname=wordpress", "root", "admin");
+    // prepare request
+    $request = $connectDatabase->prepare("SELECT * FROM post");
+    // execute request
+    $request->execute();
+    // fetch all data from table posts
+    $posts = $request->fetchAll(PDO::FETCH_ASSOC);
+?>
 
-    
+    <div>
+        <?php foreach($posts as $post): ?>
+            <h1><?= $post['categorie']; ?></h1>
+        <?php endforeach; ?>
+    </div>
 
 <?php require_once 'parts/footer.php'; ?>
