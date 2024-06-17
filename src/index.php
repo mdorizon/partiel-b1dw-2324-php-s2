d155d3d196222ed05d09abac14daeeb27bdbe5bd
MDorizon <mdorizon@proton.me>
2024-06-17 14:06:50 +0200

    :sparkles: listing des billets

--- src/index.php
+++ src/index.php
@@ -10,10 +10,34 @@
     $posts = $request->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-    <div>
-        <?php foreach($posts as $post): ?>
-            <h1><?= $post['categorie']; ?></h1>
-        <?php endforeach; ?>
-    </div>
+    <section class="d-flex flex-column align-items-center mt-5">
+        <div class="col-9">
+            <div class="d-flex justify-content-between w-100">
+                <h2>Liste des billets</h2>
+                <a type="button" class="btn btn-primary" href="#">Ajouter un billet</a>
+            </div>
+            <p>filtres</p>
+            <div class="feedback-list d-flex gap-4 mb-5 flex-wrap w-100">
+                <?php foreach($posts as $post): ?>
+                    <div class="card d-flex flex-row w-100">
+                        <div class="card-body">
+                            <h5 class="card-subtitle mb-2 text-muted">Match | <?= $post['categorie'] ?></h5>
+                            <p class="card-text"><small class="text-body-secondary">Groupe : <?=  $post['groupe'] ?></small></p>
+                            <p class="card-text"><small class="text-body-secondary"><?=  $post['equipe1'] ?> // <?=  $post['equipe2'] ?></small></p>
+                            <p class="card-text"><?=  $post['description'] ?></p>
+                            <?php $date = new DateTimeImmutable($post['date_heure']); ?>
+                            <p class="card-text"><small class="text-body-secondary"><?= $date->format('d-m-Y') ?> | <?= $date->format('h:i') ?>h | <?= $post['lieu'] ?></small></p>
+                        </div>
+                        <div class="card-body d-flex flex-column align-items-end">
+                            <h5 class="card-subtitle mb-2 text-muted"><?= $post['prix'] ?>€</h5>
+                            <div>
+                                <button type="button" class="btn btn-primary ">Voir le billet</button>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
 
 <?php require_once 'parts/footer.php'; ?>
--- src/parts/header.php
+++ src/parts/header.php
@@ -9,3 +9,8 @@
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.min.js" integrity="sha512-WW8/jxkELe2CAiE4LvQfwm1rajOS8PHasCCx+knHG0gBHt8EXxS6T6tJRTGuDQVnluuAvMxWF4j8SNFDKceLFg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 <body>
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
